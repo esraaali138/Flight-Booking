54e213aa24724d60ff645c266275e11d70a1ad64
esraaali138 <esraaali1999138@gmail.com>
2024-12-20 12:16:55 +0200

    resolve-issue

--- travel-system-task/src/app/app.routes.ts
+++ travel-system-task/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { FlightDetailsComponent } from './components/flight-details/flight-detai
 import { SearchResultsComponent } from './components/search-result/search-result.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/default', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, 
   {
     path: 'flight-details/:flightNumber',
     component: FlightDetailsComponent,
